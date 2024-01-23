52af02f584ffced800ca7bb9bd6a79db0d1690fe
Alexander Nemtsev <mrcrazy696@gmail.com>
2024-01-23 14:25:25 +0300

    HW 12 complete

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,17 +1,21 @@
-const initState = {
+type ThemeState = {
+    themeId: number;
+};
+
+const initState: ThemeState = {
     themeId: 1,
 };
 
-// TODO: убрать eslint-disable
-/* eslint-disable */
-export const themeReducer = (state = initState, action: any): any => {
-    // fix any
+export const themeReducer = (state = initState, action: Action): ThemeState => {
     switch (action.type) {
-        // дописать
+        case 'SET_THEME_ID':
+            return { themeId: action.id };
 
         default:
             return state;
     }
 };
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }); // fix any
+type Action = ReturnType<typeof changeThemeId>;
+
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id }) as const;
